48304bb1dcd6af0ef32f2eef7a41c8c7b34f71ee
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-07 08:28:42 -0500

    Some basics with handlebars

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+
 const app = express()
 const port = 3000
 
+app.engine('.hbs', exphbs({
+	defaultLayout: 'main',
+	extname: '.hbs',
+	layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+
 app.get('/', (request, response) => {
-	response.send('Hello from Express!')
-})
-
-app.listen(port, err => {
-	if (err) {
-		return console.log('something happened', err)
-	}
-
-	console.log(`server is listening on ${port}`)
+	response.render('home', {
+		name: 'John'
+	})
 })
