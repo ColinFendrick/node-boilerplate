d9f19ea181c12ee33fe450e18b59e35a75adffe7
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-07 08:19:05 -0500

    Initial commit with boilerplate, eslint and gitignore

---
+++ app/calc.js
@@ -0,0 +1,7 @@
+const sum = arr => {
+	return arr.reduce((a, b) => {
+		return a + b
+	})
+}
+
+module.exports.sum = sum
---
+++ app/index.js
@@ -0,0 +1,6 @@
+const { sum } = require('./calc')
+
+const numbersToAdd = [3,4,10,2]
+
+const result = sum(numbersToAdd)
+console.log(result)
